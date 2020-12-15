68fd72e1d6f0acc105bbb9db6ff079ef6bdaf682
Subhash Chandra <subhashchandra30720@gmail.com>
2020-12-15 09:26:42 +0530

    Task completed

--- src/Dashboard/employee.js
+++ src/Dashboard/employee.js
@@ -1,18 +1,63 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "./employee.module.css";
-import { Card, List, message, Avatar, Spin, Button } from "antd";
+import { Card, Table } from "antd";
 
 // import InfiniteScroll from "react-infinite-scroller";
 
 function Employees(props) {
+	const dataSource = [
+		{
+			key: "1",
+			employee: "Ramesh",
+			thisMonth: "1000",
+			lastMonth: "1000",
+		},
+		{
+			key: "2",
+			employee: "Suresh",
+			thisMonth: "1000",
+			lastMonth: "1000",
+		},
+		{
+			key: "3",
+			employee: "Ramesh",
+			thisMonth: 4000 + "r",
+			lastMonth: 1000 + "r",
+		},
+		{
+			key: "4",
+			employee: "Abhisek",
+			thisMonth: 4424,
+			lastMonth: 1452,
+		},
+	];
+
+	const columns = [
+		{
+			title: "Employee",
+			dataIndex: "employee",
+			key: "serviceType",
+		},
+		{
+			title: "This Month",
+			dataIndex: "thisMonth",
+			key: "thisMonth",
+		},
+		{
+			title: "Last Month",
+			dataIndex: "lastMonth",
+			key: "lastMonth",
+		},
+	];
+
 	return (
 		<Card bordered={false} className={styles.card}>
 			<div className={styles.cardHeader}>
-				<h4>
-					Details of Employees <br />
-					<small>We have best services in the world</small>
-				</h4>
+				<h4>Top Employees</h4>
 				<div className={styles.actionBtn}></div>
+			</div>
+			<div className={styles.card_content}>
+				<Table dataSource={dataSource} columns={columns} size="small" />
 			</div>
 		</Card>
 	);
--- src/Dashboard/services.js
+++ src/Dashboard/services.js
@@ -1,18 +1,63 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "./services.module.css";
-import { Card, List, message, Avatar, Spin, Button } from "antd";
+import { Card, Table } from "antd";
 
 // import InfiniteScroll from "react-infinite-scroller";
 
 function Services(props) {
+	const dataSource = [
+		{
+			key: "1",
+			serviceType: "Tatto removal",
+			thisMonth: 4,
+			lastMonth: 1,
+		},
+		{
+			key: "2",
+			serviceType: "Tatto removal",
+			thisMonth: 4,
+			lastMonth: 1,
+		},
+		{
+			key: "3",
+			serviceType: "Tatto removal",
+			thisMonth: 4,
+			lastMonth: 1,
+		},
+		{
+			key: "4",
+			serviceType: "Tatto removal",
+			thisMonth: 4,
+			lastMonth: 1,
+		},
+	];
+
+	const columns = [
+		{
+			title: "Service Type",
+			dataIndex: "serviceType",
+			key: "serviceType",
+		},
+		{
+			title: "This Month",
+			dataIndex: "thisMonth",
+			key: "thisMonth",
+		},
+		{
+			title: "Last Month",
+			dataIndex: "lastMonth",
+			key: "lastMonth",
+		},
+	];
+
 	return (
 		<Card bordered={false} className={styles.card}>
 			<div className={styles.cardHeader}>
-				<h4>
-					Our Services <br />
-					<small>We have best services in the world</small>
-				</h4>
+				<h4>Top Services</h4>
 				<div className={styles.actionBtn}></div>
+			</div>
+			<div className={styles.card_content}>
+				<Table dataSource={dataSource} columns={columns} size="small" />
 			</div>
 		</Card>
 	);
